82139c57cf4b7af249cd437b56e24dd11cb128ad
Mariano <nanoavila03@gmail.com>
2026-01-04 16:30:44 -0600

    elimando archivos dejando carpeta limpia

--- Python Basico/Ejercicios de sintaxis/5_Ejercicios.py
+++
@@ -1,6 +0,0 @@
-#Dada n cantidad de notas de un estudiante, calcular:
-#Cuantas notas tiene aprobadas (mayor a 70).
-#Cuantas notas tiene desaprobadas (menor a 70).
-#El promedio de todas.
-#El promedio de las aprobadas.
-#El promedio de las desaprobadas.
